06cb8744b6f76aa3916c9b58f433f47b06f96ac3
naveed_ashfaq <m.naveedashfaq@gmail.com>
2025-02-11 23:38:58 +0500

    reddit data added

--- scripts/reddit_scraper.py
+++ scripts/reddit_scraper.py
@@ -1,21 +1,27 @@
-import praw
-import pandas as pd
+import praw  # type: ignore
+import pandas as pd  # type: ignore
+from dotenv import load_dotenv # type: ignore
+import os
 
-# Reddit API Credentials (Replace with your own)
+load_dotenv()
+
 reddit = praw.Reddit(
-    client_id="YOUR_CLIENT_ID",
-    client_secret="YOUR_CLIENT_SECRET",
-    user_agent="crypto_scraper",
+    client_id=os.getenv("CLIENT_ID"),
+    client_secret=os.getenv("CLIENT_SECRET"),
+    user_agent=os.getenv("USER_AGENT"),
 )
 
 # Fetch posts from r/CryptoCurrency
 subreddit = reddit.subreddit("CryptoCurrency")
+print("🚀 Fetching data from Reddit...", subreddit)
 posts = []
 for post in subreddit.hot(limit=100):  # Get top 100 posts
     posts.append([post.title, post.score, post.num_comments, post.created_utc])
 
-# Save to CSV
-df = pd.DataFrame(posts, columns=["Title", "Upvotes", "Comments", "Timestamp"])
-df.to_csv("../datasets/raw/reddit_data.csv", index=False)
-
-print("✅ Reddit data saved successfully!")
+print("📊 Data fetched successfully!", posts[0])
+if posts:
+    df = pd.DataFrame(posts, columns=["Title", "Upvotes", "Comments", "Timestamp"])
+    df.to_csv("./datasets/raw/reddit_data.csv", index=False)
+    print("✅ Reddit data saved successfully!")
+else:
+    print("❌ No data fetched from Reddit!")
